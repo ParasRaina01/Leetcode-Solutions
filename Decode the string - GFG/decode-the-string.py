d695af30dd536242a71575f38636d111d41ed6ba
Paras Raina <90261069+ParasRaina01@users.noreply.github.com>
2022-10-11 21:51:16 +0530

    Added solution - LeetHub

---
+++ Decode the string - GFG/decode-the-string.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    def decodedString(self, s):
+        # code here
+        stack = []; curNum = 0; curString = ''
+        for c in s:
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num*curString
+            elif c.isdigit():
+                curNum = curNum*10 + int(c)
+            else:
+                curString += c
+        return curString 
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input()
+        
+        ob = Solution()
+        print(ob.decodedString(s))
+# } Driver Code Ends
